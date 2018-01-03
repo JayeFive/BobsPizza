c0573df17fc947fe8f6101f673906bff9ae5fba8
JayeFive <john.olson3@gmail.com>
2018-01-03 03:22:39 +0000

    Finish invoice functionality. Needs bug testing for click off event.

--- receiptGenerator.js
+++ receiptGenerator.js
@@ -30,14 +30,36 @@ function findTotalPrice () {
             break;
     }   
     
-    //Determine if the stuffed crust(crust3) has been checked
-    if (document.getElementById("crust3").checked == true) {
-        totalPrice += 3;
+    
+    //Determine crust type line item
+    var crusts = document.getElementsByName("crust");
+    for (var i = 0; i < crusts.length; i++) {
+        if (crusts[i].checked == true && crusts[i].id == "crust3") { //crust3(stuffed crust) costs extra
+            generateOptionElem(crusts[i].value, "$3");
+            totalPrice += 3;
+        } else if (crusts[i].checked == true && crusts[i].id != "crust3")
+            generateOptionElem(crusts[i].value, "$0");
     }
     
-    //Determine if extra cheese has been checked
-    if (document.getElementById("cheese3").checked == true)
-        totalPrice += 3;
+    
+    //Determine extra cheese line item
+    var cheeses = document.getElementsByName("cheese");
+    for (var i = 0; i < cheeses.length; i++) {
+        if (cheeses[i].checked == true && cheeses[i].id == "cheese3") { //cheese3(extra cheese) costs extra
+            generateOptionElem(cheeses[i].value, "$3");
+            totalPrice += 3;
+        } else if (cheeses[i].checked == true && cheeses[i].id != "cheese3")
+            generateOptionElem(cheeses[i].value, "$0");
+    }
+    
+
+    //Determine sauce line item
+    var sauces = document.getElementsByName("sauce");
+    for (var i = 0; i < sauces.length; i++) {
+        if (sauces[i].checked == true)
+            generateOptionElem(sauces[i].value, "$0");
+    }
+        
         
         
     //Determine extra toppings -- first meat and first veggie are free
@@ -47,13 +69,23 @@ function findTotalPrice () {
     var veggies = document.getElementsByName("veggies");
     
     for (var i = 0; i < meats.length; i++) {
-        if (meats[i].checked == true)
+        if (meats[i].checked == true) {
             numMeats++;
+            if (numMeats <= 1)
+                generateOptionElem(meats[i].value, "$0");
+            else
+                generateOptionElem(meats[i].value, "$1");
+        }
     }
     
     for (var i = 0; i < veggies.length; i++) {
-        if (veggies[i].checked == true)
+        if (veggies[i].checked == true) {
             numVeggies++;
+            if (numVeggies <= 1)
+                generateOptionElem(veggies[i].value, "$0");
+            else
+                generateOptionElem(veggies[i].value, "$1");
+        }
     }
     
     if (numMeats - 1 >= 1) 

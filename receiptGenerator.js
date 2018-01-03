cd54ca7d0343bc83c4f98ac3ddd070efba2ac887
JayeFive <john.olson3@gmail.com>
2018-01-03 02:49:25 +0000

    Add invoice first functionality. Cancel button clears the invoice.

--- receiptGenerator.js
+++ receiptGenerator.js
@@ -14,21 +14,26 @@ function findTotalPrice () {
     switch (orderForm.pizzaSize.value) {
         case "size1": 
             totalPrice += 6;
+            generateOptionElem("Personal Pizza", "$6");
             break;
         case "size2":
             totalPrice += 10;
+            generateOptionElem("Medium Pizza", "$10");
             break;
         case "size3":
             totalPrice += 14;
+            generateOptionElem("Large Pizza", "$14");
             break;
         case "size4":
             totalPrice += 16;
+            generateOptionElem("Extra Large Pizza", "$16");
             break;
     }   
     
     //Determine if the stuffed crust(crust3) has been checked
-    if (document.getElementById("crust3").checked == true)
+    if (document.getElementById("crust3").checked == true) {
         totalPrice += 3;
+    }
     
     //Determine if extra cheese has been checked
     if (document.getElementById("cheese3").checked == true)
@@ -57,4 +62,30 @@ function findTotalPrice () {
         totalPrice += (numVeggies - 1);
     
     console.log(totalPrice);
+    
+    generateOptionElem("", "");
+    generateOptionElem("<strong>Total</strong>", "$" + totalPrice)
+}
+
+
+
+function generateOptionElem (option, price) {
+    var invoice = document.getElementById("invoice");
+    var newRow = document.createElement("tr");
+
+    newRow.innerHTML = option;
+    invoice.appendChild(newRow);
+    
+    var newPriceElem = document.createElement("td");
+    
+    newPriceElem.innerHTML = price;
+    newRow.appendChild(newPriceElem);
+}
+
+function clearInvoice () {
+    var elements = document.getElementsByTagName("td");
+    while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
+    
+    elements = document.getElementsByTagName("tr");
+    while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
 }

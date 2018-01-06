db29fe971274516ef21bbcc18d4c26818c2433bb
JayeFive <john.olson3@gmail.com>
2018-01-06 02:04:31 +0000

    Refactor checkbox update function

--- index.js
+++ index.js
@@ -9,32 +9,25 @@ const prices = {
   pizzaSauce: [0, 0, 0, 0]
 };
 
-
-// Event handler for the refresh button in the navbar
-document.getElementById('refresh-button').addEventListener("click", resetForm, false);
-
-
 // Event handler for radio button changes
 var radioButtonLabels = document.getElementsByClassName("radio-btn-label");
 
-// Add event listener for each radio button
 for (var i = 0; i < radioButtonLabels.length; i++) {
   
-  radioButtonLabels[i].addEventListener('click', function () {
+  radioButtonLabels[i].addEventListener('click', function() {
     updateRadioButtonPrice (this.firstElementChild.id.slice(-1), this.firstElementChild.name);
   }, false);
   
 }
 
+// Event handler for the refresh button in the navbar
+document.getElementById('refresh-button').addEventListener("click", resetForm, false);
 
 // Manually set all radio buttons to their default value and clear all checkboxes and price boxes
 function resetForm() {
   console.log('this worked');
   document.getElementById("pizzaSize3").checked = true;
 }
-
-
-
 
 
 function clearInvoice () {
@@ -45,9 +38,7 @@ function clearInvoice () {
   while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
 }
 
-
-
-
+// Live update of price boxes next to radio buttons
 function updateRadioButtonPrice (priceId, elemName) {
     
   // Set the correct price from the prices object
@@ -61,32 +52,34 @@ function updateRadioButtonPrice (priceId, elemName) {
   }
 }
 
+var totalExtraPrice = 0;
+
+// Live update of price boxes next to checkboxes
 function updateCheckedButtonPrice (priceId, elemName) {
   // Find the number of checked checkboxes
   var numChecked = getCheckedBoxes(elemName);
   var priceBoxes = document.getElementsByClassName(elemName);
   var changedElement = document.getElementById(elemName + "Price" + priceId);
-  var totalExtraPrice = 0;
-    
   //Check if the user just selected or deselected a topping
   if (document.getElementById(elemName + priceId).checked) {
     // Check if the first free topping has already been selected 
-    if (numChecked > 1) {
-      // If so, add a dollar to the newly selected topping
+    if (numChecked > 1) {     // If so, add a dollar to the newly selected topping
       changedElement.innerHTML = (1).toFixed(2);
+      totalExtraPrice++;
     }   // If first free topping has not been selected, do nothing
   } else { // if the user just deselected a topping, firstly set that price box to 0.00
     changedElement.innerHTML = (0).toFixed(2);
-    for (var i = 1; i <= priceBoxes.length; i++) {
-      totalExtraPrice += Number(document.getElementById(elemName + "Price" + i).innerHTML);
-    }   //Compare the number of selected toppings with the total extra price calculated in the for loop above
+    totalExtraPrice--;
+    //Compare the number of selected toppings with the total extra price 
     if(totalExtraPrice === numChecked) {
       // Search through the prices to find the first one that's not 0.00, clear it and leave the function
       for (var i = 0; i < priceBoxes.length; i++) {
-        if (Number(priceBoxes[i].innerHTML) == 1) {
+        
+        if (Number(priceBoxes[i].innerHTML) === 1) {
           priceBoxes[i].innerHTML = (0).toFixed(2);
           return;
         }
+        
       }
     }   
   }
